fc4a54626ae9aac52af3a04e81be3c990119da5c
chintan <chintan@gmail.com>
2025-09-23 18:19:31 +0530

    all

---
+++ A001_HibernateCore/src/main/java/com/AddStudent.java
@@ -0,0 +1,38 @@
+package com;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class AddStudent {
+	public static void main(String[] args) {
+		
+		
+		Student st = new Student();
+		st.setId(1);
+		st.setName("Bansi");
+		st.setEmail("bansi@gmail.com");
+		
+		
+		
+		Configuration cfg = new Configuration();
+		cfg = cfg.configure("hibernate.cfg.xml");
+		cfg = cfg.addAnnotatedClass(Student.class);
+		
+		
+		SessionFactory sf = cfg.buildSessionFactory();
+		
+		
+		Session s  =sf.openSession();
+		Transaction tx = s.beginTransaction();
+		s.saveOrUpdate(st);
+		
+		tx.commit();
+		
+		
+		
+		
+		
+	}
+}
