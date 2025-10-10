bbefaaee9c02d1456209d9c78eb32efd9cbb16d4
chintan <chintan@gmail.com>
2025-10-10 17:44:56 +0530

    all

---
+++ Z003_Springboot-web/src/main/java/com/example/demo/Z003SpringbootWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Z003SpringbootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Z003SpringbootWebApplication.class, args);
+	}
+
+}
